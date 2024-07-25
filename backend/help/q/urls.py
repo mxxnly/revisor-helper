ab9df177e917bde09963704c03a7961c2a034a0a
mxxnly <makssklada@gmail.com>
2024-07-25 23:07:12 +0300

    Added button delete and edit to shop list

--- backend/help/q/urls.py
+++ backend/help/q/urls.py
@@ -3,5 +3,8 @@ from django.urls import path
 
 urlpatterns = [
     path('shops/', views.shop_list, name='shop_list'),
+    path('shops/add/', views.shop_add, name='shop_add'),
+    path('shops/edit/<int:shop_id>/', views.shop_edit, name='shop_edit'),
+    path('shops/delete/<int:shop_id>/', views.shop_delete, name='shop_delete'),
     path('reorder_shops/', views.reorder_shops, name='reorder_shops'),
 ]
--- backend/help/q/views.py
+++ backend/help/q/views.py
@@ -1,9 +1,10 @@
 
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 import json
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from main.models import Shop
+from .forms import ShopForm
 
 def shop_list(request):
     shops = Shop.objects.all()
@@ -18,3 +19,36 @@ def reorder_shops(request):
         for item in data['order']:
             Shop.objects.filter(id=item['id']).update(position=item['position'])
         return JsonResponse({'status': 'ok'})
+    
+
+def shop_list(request):
+    shops = Shop.objects.all()
+    return render(request, 'q.html', {'shops': shops})
+
+def shop_add(request):
+    if request.method == 'POST':
+        form = ShopForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('shop_list')
+    else:
+        form = ShopForm()
+    return render(request, 'shop_add.html', {'form': form})
+
+def shop_edit(request, shop_id):
+    shop = Shop.objects.get(id=shop_id)
+    if request.method == 'POST':
+        form = ShopForm(request.POST, instance=shop)
+        if form.is_valid():
+            form.save()
+            return redirect('shop_list')
+    else:
+        form = ShopForm(instance=shop)
+    return render(request, 'shop_edit.html', {'form': form})
+
+def shop_delete(request, shop_id):
+    shop = Shop.objects.get(id=shop_id)
+    if request.method == 'POST':
+        shop.delete()
+        return redirect('shop_list')
+    return render(request, 'shop_delete.html', {'shop': shop})
