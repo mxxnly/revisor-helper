bac99658ecce3df4b113eb8fa260fdd19853d5fc
mxxnly <maksskladan@gmail.com>
2024-09-11 13:28:55 +0000

    Updated server settings

--- backend/help/help/settings.py
+++ backend/help/help/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent.parent.parent
 SECRET_KEY = 'django-insecure-s@%h*%y#x4goqtzt&mf6essioxinx7xu@oyvvmlx6zgd%h0+(2'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
      'localhost', 'mxxnly.cloud', '62.72.22.94','127.0.0.1'
