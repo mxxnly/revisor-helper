dda133ba5cad27c947eb741bd1510ddb8a26dc98
root <root@srv587073.hstgr.cloud>
2025-01-16 20:34:49 +0000

    Update server

--- backend/help/counting/models.py
+++ backend/help/counting/models.py
@@ -2,6 +2,8 @@ from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
 from decimal import Decimal
+from django.db.models import F
+
 
 
 class WorkLog(models.Model):
@@ -17,6 +19,16 @@ class WorkLog(models.Model):
             additional_hours = self.minutes_worked // 60
             self.hours_worked += additional_hours
             self.minutes_worked = self.minutes_worked % 60
+            
+    def save(self, *args, **kwargs):
+        is_new = self.pk is None
+        super().save(*args, **kwargs)
+
+        if self.was_on_far_point and is_new:
+            revisor = self.user.revisor
+            if revisor:
+                revisor.way_shops += 1
+                revisor.save()
 
     def __str__(self):
         return f"{self.user.username} - {self.date} - {self.hours_worked} годин, {self.minutes_worked} хвилин."
